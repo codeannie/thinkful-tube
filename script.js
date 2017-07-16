4cbf62a1137db92465e4bd192f1a456b01b4ea3f
Annie Chiang <codepanduh@gmail.com>
2017-07-16 14:46:37 -0700

    updating styling, title and script

--- script.js
+++ script.js
@@ -22,26 +22,26 @@ var getYoutubeResults = function(searchTerm) {
 
 //render search results
 function renderVideos(data) {
-
+    $(".results-display").empty();
     for (i = 0; i < data.items.length; i+=1) {
-        // const item = data.items[i];
-        const videoSnippet = data.items[i].snippet;
-        const videoId = data.items[i].id.videoId;
+        const item = data.items[i];
+        const videoSnippet = item.snippet;
+        const videoId = item.id.videoId;
         const videoTitle = videoSnippet.title;
         const videoDesc = videoSnippet.description;
         const videoThumbnail = videoSnippet.thumbnails.default.url; 
         const videoChannelTitle = videoSnippet.channelTitle; 
         const videoChannelId = videoSnippet.channelId; 
         const newElem = (
-            `<div class="col-4">  
-            <div class="video">
-                <a href="https://www.youtube.com/watch?v=${videoId}">
+            // `<div class="col-4">  
+            `<div class="video">
+                <a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">
                     <h3 class="video-title">${videoTitle}</h3>
                     <img class="thumbnail" src="${videoThumbnail}" alt="${videoDesc}">
                     <p class="video-description">${videoDesc}</p>
                 </a>
-            </div>
-            </div>`
+             </div>`
+              //  </div>` 
         );
         $(".results-display").append(newElem);
   }
